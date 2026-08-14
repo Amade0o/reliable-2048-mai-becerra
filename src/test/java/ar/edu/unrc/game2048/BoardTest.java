aae8946ee09ea7a277bc9c99bc17d35cfddce897
AgustinCoup <tutyboss12@gmail.com>
2026-08-14 11:38:16 -0300

    Añado más test para la clase Board, clases públicas testeadas hasta isLosingBoard(). Solucion de bug en ese útimo método, closed #4

--- src/test/java/ar/edu/unrc/game2048/BoardTest.java
+++ src/test/java/ar/edu/unrc/game2048/BoardTest.java
@@ -1,4 +1,6 @@
 package ar.edu.unrc.game2048;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
@@ -6,6 +8,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
+
+import ar.edu.unrc.game2048.Board.Position;
 
 public class BoardTest {
     /*Necesary method, wich is used to make manual boards */
@@ -35,6 +39,30 @@ public class BoardTest {
 
         board.setCell(1, 1, cell1);
         board.setCell(2, 2, cell2);
+
+        return board;
+    }
+
+    /*Private void for creating a 4x4 board with "2" and "4" looking like this:*/
+    // 2 4 2 4
+    // 4 2 4 2
+    // 2 4 2 4
+    // 4 2 4 2
+    private Board createFullLosingBoard(){
+        Board board = createEmptyBoard();
+        Cell cell1 = new Cell(2);
+        Cell cell2 = new Cell(4);
+
+        //Filling the board with a pattern that makes it impossible to merge any cells
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                if((i+j) % 2 == 0){
+                    board.setCell(i, j, cell1);
+                }else{
+                    board.setCell(i, j, cell2);
+                }
+            }
+        }
 
         return board;
     }
@@ -100,7 +128,7 @@ public class BoardTest {
         int expectedSize = 4;
         Board board = new Board();
  
-        //AC
+        //ACT
         int gettedSize = board.getSize();
         
         //ASSERT
@@ -113,11 +141,258 @@ public class BoardTest {
         int expectedScore = 0;
         Board board = new Board();
  
-        //AC
+        //ACT
         int gettedScore = board.getScore();
         
         //ASSERT
         assertTrue(gettedScore == expectedScore);
+    }
+
+    @Test
+    public void getCellInBoard(){
+        //Arrange
+        Board board = createBoardTest1();
+        Cell expectedCell = new Cell(2);
+ 
+        //Act
+        Cell gettedCell = board.getCell(1, 1);
+        
+        //Assert
+        assertTrue(gettedCell.equals(expectedCell));
+    }
+
+    @Test
+    public void getCellOutOfBoard(){
+        //Arrange
+        Board board = createBoardTest1();
+        
+        //Assert
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            board.getCell(5, 5);
+        });
+    }
+
+    @Test
+    public void setCellInBoard(){
+        //Arrange
+        Board board = createBoardTest1();
+        Cell expectedCell = new Cell(4);
+ 
+        //Act
+        board.setCell(1, 1, expectedCell);
+        Cell gettedCell = board.getCell(1, 1);
+        
+        //Assert
+        assertTrue(gettedCell.equals(expectedCell));
+    }
+
+    @Test
+    public void setCellOutOfBoard(){
+        //Arrange
+        Board board = createBoardTest1();
+        Cell expectedCell = new Cell(4);
+        
+        //Assert
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            board.setCell(5, 5, expectedCell);
+        });
+    }
+
+    @Test
+    public void setCellNull(){
+        //Arrange
+        Board board = createBoardTest1();
+        
+        //Assert
+        assertThrows(IllegalArgumentException.class, () -> {
+            board.setCell(1, 1, null);
+        });
+    }
+
+    @Test
+    public void getEmptyPositions(){
+        //Arrange
+        Board board = createBoardTest1();
+        Set<Position> expectedEmptyPositions = new HashSet<>();
+        //Primera fila
+        expectedEmptyPositions.add(new Position(0, 0));
+        expectedEmptyPositions.add(new Position(0, 1));
+        expectedEmptyPositions.add(new Position(0, 2));
+        expectedEmptyPositions.add(new Position(0, 3));
+        //Segunda fila
+        expectedEmptyPositions.add(new Position(1, 0));
+        expectedEmptyPositions.add(new Position(1, 2));
+        expectedEmptyPositions.add(new Position(1, 3));
+        //Tercera fila
+        expectedEmptyPositions.add(new Position(2, 0));
+        expectedEmptyPositions.add(new Position(2, 1));
+        expectedEmptyPositions.add(new Position(2, 3));
+        //Cuarta fila
+        expectedEmptyPositions.add(new Position(3, 0));
+        expectedEmptyPositions.add(new Position(3, 1));
+        expectedEmptyPositions.add(new Position(3, 2));
+        expectedEmptyPositions.add(new Position(3, 3));
+
+        //Act
+        Set<Position> gettedEmptyPositions = board.getEmptyPositions();
+        
+        //Assert
+        assertTrue(gettedEmptyPositions.equals(expectedEmptyPositions));
+    }
+
+    @Test
+    public void hasEmptyCells(){
+        //Arrange
+        Board board = createBoardTest1();
+ 
+        //Act
+        boolean hasEmptyCells = board.hasEmptyCells();
+        
+        //Assert
+        assertTrue(hasEmptyCells);
+    }
+
+    @Test
+    public void hasNoEmptyCells(){
+        //Arrange
+        Board board = new Board();
+        int size = board.getSize();
+
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                Cell cell = new Cell(2);
+                board.setCell(i, j, cell);
+            }
+        }
+
+        //Act
+        boolean hasEmptyCells = board.hasEmptyCells();
+        
+        //Assert
+        assertFalse(hasEmptyCells);
+    }
+
+    @Test
+    public void isWinningBoard(){
+        //Arrange
+        Board board = createEmptyBoard();
+        Cell cell1 = new Cell(2048);
+        board.setCell(1, 1, cell1);
+
+        //Act
+        boolean isWinningBoard = board.isWinningBoard();
+        
+        //Assert
+        assertTrue(isWinningBoard);
+    }
+
+    @Test
+    public void isNotWinningBoard(){
+        //Arrange
+        Board board = createEmptyBoard();
+        Cell cell1 = new Cell(2);
+        board.setCell(1, 1, cell1);
+
+        //Act
+        boolean isWinningBoard = board.isWinningBoard();
+        
+        //Assert
+        assertFalse(isWinningBoard);
+    }
+
+    @Test
+    public void emptyIsNotLosingBoard(){
+        //Arrange
+        Board board = createEmptyBoard();
+        //Act
+        boolean isLosingBoard = board.isLosingBoard();
+        //Assert
+        assertFalse(isLosingBoard);
+    }
+
+    @Test
+    public void upMergeIsNotLosingBoard(){
+        //Arrange
+        Board board = createFullLosingBoard();
+        Cell cell1 = new Cell(2);
+        Cell cell2 = new Cell(8);
+        //Set this board to look like this (there is only an up merge possible):
+        // 2 4 2 4
+        // 2 8 4 2
+        // 8 4 2 4
+        // 4 2 4 2
+        board.setCell(1, 0, cell1);
+        board.setCell(2, 0, cell2);
+        board.setCell(1, 1, cell2);
+
+        //Act
+        boolean isLosingBoard = board.isLosingBoard();
+
+        //Assert
+        assertFalse(isLosingBoard);
+    }
+
+    @Test
+    public void rightMergeIsNotLosingBoard(){
+        //Arrange
+        Board board = createFullLosingBoard();
+        Cell cell1 = new Cell(2);
+        Cell cell2 = new Cell(8);
+        //Set this board to look like this (there is only an right merge possible):
+        // 2 4 2 2
+        // 4 2 4 8
+        // 2 4 2 4
+        // 4 2 4 2
+        board.setCell(0, 3, cell1);
+        board.setCell(1, 3, cell2);
+
+        //Act
+        boolean isLosingBoard = board.isLosingBoard();
+
+        //Assert
+        assertFalse(isLosingBoard);
+    }
+
+    @Test
+    public void downMergeIsNotLosingBoard(){
+        //Arrange
+        Board board = createFullLosingBoard();
+        Cell cell1 = new Cell(2);
+        Cell cell2 = new Cell(8);
+        //Set this board to look like this (there is only an down merge possible):
+        // 2 4 2 4
+        // 4 2 4 2
+        // 2 4 2 4
+        // 2 8 4 2
+        board.setCell(3, 0, cell1);
+        board.setCell(3, 1, cell2);
+
+        //Act
+        boolean isLosingBoard = board.isLosingBoard();
+
+        //Assert
+        assertFalse(isLosingBoard);
+    }
+
+    @Test
+    public void leftMergeIsNotLosingBoard(){
+        //Arrange
+        Board board = createFullLosingBoard();
+        Cell cell1 = new Cell(2);
+        Cell cell2 = new Cell(8);
+        //Set this board to look like this (there is only an left merge possible):
+        // 2 4 2 4
+        // 4 2 4 2
+        // 8 4 2 4
+        // 2 2 4 2
+        board.setCell(3, 0, cell1);
+        board.setCell(2, 0, cell2);
+
+        //Act
+        boolean isLosingBoard = board.isLosingBoard();
+
+        //Assert
+        assertFalse(isLosingBoard);
     }
 }
 
