3e61f8fa075afa4870c3555a4a5f9b21cbed7f4b
Amade0o <amadeoemai@gmail.com>
2026-09-22 10:35:01 -0300

    Folders

--- scripts-configs/runRandoop.sh
+++ scripts-configs/runRandoop.sh
@@ -9,8 +9,8 @@ java -cp "lib/randoop-all-4.3.4.jar:target/classes" \
   --testclass=ar.edu.unrc.game2048.Board \
   --testclass=ar.edu.unrc.game2048.Cell \
   --testclass=ar.edu.unrc.game2048.RandomNumGeneration.DeterministicRandom \
-  --omit-classes-file=randoop-omit-classes.txt \
-  --omit-methods-file=randoop-omit-methods.txt \
+  --omit-classes-file=scripts-configs \ randoop-omit-classes.txt \
+  --omit-methods-file=scripts-configs \ randoop-omit-methods.txt \
   --time-limit=10 \
   --junit-output-dir=src/test/java \
   --junit-package-name=randoopTests
