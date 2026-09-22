710b422261862d1e98d0c33337f3a192e208cd25
AgustinCoup <tutyboss12@gmail.com>
2026-09-22 16:17:04 -0300

    Corro los test de record contra los generados por evosuite y rundoop exclusivamente

--- scripts-configs/runRandoop.sh
+++ scripts-configs/runRandoop.sh
@@ -4,13 +4,18 @@ set -e
 mvn clean compile
 rm -rf src/test/java/randoopTests
 
-java -cp "lib/randoop-all-4.3.4.jar:target/classes" \
+CP_SEP=":"
+case "$(uname -s)" in
+  MINGW*|MSYS*|CYGWIN*) CP_SEP=";" ;;
+esac
+
+java -cp "lib/randoop-all-4.3.4.jar${CP_SEP}target/classes" \
   randoop.main.Main gentests \
   --testclass=ar.edu.unrc.game2048.Board \
   --testclass=ar.edu.unrc.game2048.Cell \
   --testclass=ar.edu.unrc.game2048.RandomNumGeneration.DeterministicRandom \
-  --omit-classes-file=scripts-configs \ randoop-omit-classes.txt \
-  --omit-methods-file=scripts-configs \ randoop-omit-methods.txt \
+  --omit-classes-file=scripts-configs/randoop-omit-classes.txt \
+  --omit-methods-file=scripts-configs/randoop-omit-methods.txt \
   --time-limit=10 \
   --junit-output-dir=src/test/java \
   --junit-package-name=randoopTests
